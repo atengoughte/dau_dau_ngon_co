0c2bfe8c3a6ffcc40580c80426e07443ae45a1ba
atengoughte <142688687+atengoughte@users.noreply.github.com>
2023-11-22 19:48:27 +0700

    up

--- b-o-m-t-o-o-n.meta.js
+++ b-o-m-t-o-o-n.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         b-o-m-t-o-o-n
 // @namespace    https://github.com/atengoughte/dau_dau_ngon_co/raw/main/b-o-m-t-o-o-n.user.js
-// @version      2023.08.20
+// @version      2023.11.22
 // @description  down b-o-m-t-o-o-n
 // @author       You
-// @match        https://www.bomtoon.com/comic/ep_view/*/*
+// @match        https://www.bomtoon.com/viewer/*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=www.bomtoon.com
 // @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
 // @grant        none
