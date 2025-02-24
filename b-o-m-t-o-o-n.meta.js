dd65e52859b62d71a1287006d66c0a2014325a75
atengoughte <142688687+atengoughte@users.noreply.github.com>
2025-02-24 22:17:20 +0700

    init

---
+++ b-o-m-t-o-o-n.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         b-o-m-t-o-o-n
+// @namespace    b-o-m-t-o-o-n.user.js
+// @version      2025.02.24
+// @description  down b-o-m-t-o-o-n
+// @author       You
+// @include      /^https:\/\/www.b[o0]mt[o0][o0]n.c[o0]m\/vi[e3]w[e3]r\/.+?\/.+/
+// @icon         https://raw.githubusercontent.com/atengoughte/dau_dau_ngon_co/main/b-o-m-t-o-o-n.png
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.48/dist/zip.min.js
+// @grant        none
+// @downloadURL  https://github.com/atengoughte/dau_dau_ngon_co/raw/main/b-o-m-t-o-o-n.user.js
+// @updateURL    https://github.com/atengoughte/dau_dau_ngon_co/raw/main/b-o-m-t-o-o-n.meta.js
+// ==/UserScript==
